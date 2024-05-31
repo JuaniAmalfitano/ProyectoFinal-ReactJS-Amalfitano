4891a540cb8ad15d3a038c45ac073ad35e1cddc7
juaniamalfi <juani.amalfitano14@gmail.com>
2024-05-31 17:55:00 -0300

    tercer commit

--- src/BannerInfo.jsx
+++ src/BannerInfo.jsx
@@ -1,20 +1,23 @@
 // BannerInfo.jsx
 import './EstiloNavBar.css';
+import transporte from "../public/Logos/transporte.png";
+import porcentaje from "../public/Logos/porcentaje.png";
+import tarjeta from "../public/Logos/tarjeta-de-credito.png";
 
 const BannerInfo = () => {
     return (
         <>
             <div className="logos-container text-center mb-4 mt-2">
                 <div className="logo-item">
-                    <img src="public\Logos\porcentaje.png" alt="Logo 1" />
+                    <img src={porcentaje} alt="Logo 1" />
                     <p><b>10% de descuento</b> Por transferencia bancaria</p>
                 </div>
                 <div className="logo-item">
-                    <img src="public\Logos\tarjeta-de-credito.png" alt="Logo 2" />
+                    <img src={tarjeta} alt="Logo 2" />
                     <p><b>Todos los medios de pago</b></p>
                 </div>
                 <div className="logo-item">
-                    <img src="public\Logos\transporte.png" alt="Logo 3" />
+                    <img src={transporte} alt="Logo 3" />
                     <p><b>Envíos a todo el país</b> Gratis a partir de $90.000</p>
                 </div>
             </div>
